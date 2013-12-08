6aa3d777b322b6440422dcd902dfe4d36100fa49
Madison Treat <madtreat@gmail.com>
2013-12-08 15:50:19 -0600

    Added several new layouts and classes for testing, added new ScanActivity

--- RenScan/src/com/example/renscan/Welcome.java
+++ RenScan/src/com/example/renscan/Welcome.java
@@ -2,7 +2,9 @@ package com.example.renscan;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Welcome extends Activity {
 
@@ -17,6 +19,11 @@ public class Welcome extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_welcome, menu);
         return true;
+    }
+    
+    public void startScanActivity(View view) {
+    	Intent intent = new Intent(this, ScanActivity.class);
+    	startActivity(intent);
     }
     
 }
