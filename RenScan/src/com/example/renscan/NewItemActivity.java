8ddc79f45d1d3e25dcbf23559e2cbe7865d726be
Kira Jones <kmystic524@tamu.edu>
2013-12-08 23:21:41 -0600

    Added view for adding items
    Modified backgrounds to existing views,
    Modified NewItemActivity,
    Currently 'Inventory button' points to my NewItemActivity because I
    couldn't test it via the picture taking since I could never get it to
    work. Also, I spent a long time trying to center the text in the buttons
    but no luck...

--- RenScan/src/com/example/renscan/NewItemActivity.java
+++ RenScan/src/com/example/renscan/NewItemActivity.java
@@ -6,30 +6,47 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 public class NewItemActivity extends Activity {
-	
+	private Spinner spinner1, spinner2, spinner3, spinner4;
+	private Button submit;
 	private String imagePath;
+	private ImageView image;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_new_item);
-		// Show the Up button in the action bar.
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+		setContentView(R.layout.category);
+		
+		//getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		
+		//addImage();
+		
+		addListenerOnButton();
+		addListenerOnSpinnerItemSelection();
+		 //Show the Up button in the action bar.
+		
 		
 		// get the intent to get the image path
-		Intent intent = getIntent();
-		imagePath = intent.getExtras().getString("imagePath");
+		//Intent intent = getIntent();
+		//imagePath = intent.getExtras().getString("imagePath");
+	
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_new_item, menu);
 		return true;
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -46,5 +63,48 @@ public class NewItemActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	 public void addImage() {
+		 	int res = getResources().getIdentifier(imagePath, "drawable", this.getPackageName());
+			image = (ImageView) findViewById(R.id.imageView1);
+			image.setImageResource(res);
+		  }
+
+	public void addListenerOnSpinnerItemSelection() {
+    	spinner1 = (Spinner) findViewById(R.id.spinner1);
+    	spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    	spinner2 = (Spinner) findViewById(R.id.spinner2);
+    	spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    	spinner3 = (Spinner) findViewById(R.id.spinner3);
+    	spinner3.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    	spinner4 = (Spinner) findViewById(R.id.spinner4);
+    	spinner4.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+      }
+     
+      // get the selected dropdown list value
+	  public void addListenerOnButton() {
+	 
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		spinner2 = (Spinner) findViewById(R.id.spinner2);
+		spinner3 = (Spinner) findViewById(R.id.spinner3);
+		spinner4 = (Spinner) findViewById(R.id.spinner4);
+		submit = (Button) findViewById(R.id.submit);
+	 
+		submit.setOnClickListener(new OnClickListener() {
+	 
+		  @Override
+		  public void onClick(View v) {
+	 
+		    Toast.makeText(NewItemActivity.this,
+			"OnClickListener : " + 
+	            "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) + 
+	            "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()) +
+	            "\nSpinner 3 : "+ String.valueOf(spinner3.getSelectedItem()) +
+	            "\nSpinner 4 : "+ String.valueOf(spinner4.getSelectedItem()),
+				Toast.LENGTH_SHORT).show();
+		  }
+	 
+		});
+	  }
 
 }
--- RenScan/src/com/example/renscan/Welcome.java
+++ RenScan/src/com/example/renscan/Welcome.java
@@ -3,15 +3,29 @@ package com.example.renscan;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class Welcome extends Activity {
 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Button b1 = (Button) findViewById(R.id.button1);
+        Button b2 = (Button) findViewById(R.id.button2);  
+        Button b3 = (Button) findViewById(R.id.button3);  
+        TextView t1 = (TextView) findViewById(R.id.textView1);  
+        Typeface font = Typeface.createFromAsset(getAssets(), "Chantelli_Antiqua.ttf");  
+        b1.setTypeface(font);  
+        b2.setTypeface(font);  
+        b3.setTypeface(font);  
+        t1.setTypeface(font);  
     }
 
     @Override
@@ -30,5 +44,12 @@ public class Welcome extends Activity {
     	Intent intent = new Intent(this, InventoryActivity.class);
     	startActivity(intent);
     }
+    /* Testing my page - Kira*/
+    public void startNewItemActivity(View view) {
+    	Intent intent = new Intent(this, NewItemActivity.class);
+    	startActivity(intent);
+    }
     
+    
+
 }
